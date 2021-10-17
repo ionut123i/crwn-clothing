c7849eafbf1065bd0324b9c31fb730136bacdf59
yoonut <pisu.ionut@ymail.com>
2021-10-17 12:59:36 +0300

    init

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
 import shopReducer from "./shop/shop.reducer";
+
 const persistConfig={
     key:'root',
     storage,
